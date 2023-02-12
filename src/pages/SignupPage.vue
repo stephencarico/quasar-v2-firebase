79ec10598e55a4cb32529d46d54c5375d594ce84
Stephen Carico <32233894+stephencarico@users.noreply.github.com>
2023-02-11 20:55:30 -0700

    feat: create a signup page

---
+++ src/pages/SignupPage.vue
@@ -0,0 +1,37 @@
+<template>
+  <q-page class="flex flex-center bg-grey-10">
+    <q-card style="width: 80%">
+      <form @submit="signupUser">
+        <q-card-section>
+          <div class="text-h6">Signup</div>
+        </q-card-section>
+        <q-card-section class="q-gutter-y-md">
+          <q-input v-model="signupForm.email" outlined label="email" />
+          <q-input
+            v-model="signupForm.password"
+            outlined
+            label="password"
+            type="password"
+          />
+          <q-btn color="primary" class="full-width" type="submit">
+            Sign Up
+          </q-btn>
+        </q-card-section>
+      </form>
+    </q-card>
+  </q-page>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const signupForm = ref({
+  email: "",
+  password: "",
+});
+
+const signupUser = () => {
+  console.log("SIGNING UP USER");
+  console.log(signupForm.value);
+};
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -7,6 +7,13 @@ const routes = [
       { path: '', component: () => import('pages/IndexPage.vue') }
     ]
   },
+  {
+    path: '/signup',
+    component: () => import('layouts/FormLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/SignupPage.vue') }
+    ]
+  },
 
   // Always leave this as last one,
   // but you can also remove it
