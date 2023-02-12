14197a04a4900fb33e5f5086494d8a8d958ab4b3
Stephen Carico <32233894+stephencarico@users.noreply.github.com>
2023-02-11 20:24:25 -0700

    init firebase

---
+++ src/boot/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCQ9SaG5RCwXYKvEtDZwsN8qr0343hFLGg",
+  authDomain: "copq-calculator.firebaseapp.com",
+  projectId: "copq-calculator",
+  storageBucket: "copq-calculator.appspot.com",
+  messagingSenderId: "603543406443",
+  appId: "1:603543406443:web:5a23582974edbe86ab6e2e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
